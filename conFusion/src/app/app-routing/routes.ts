fdbf9d80a40a276afaa8e61c893e5b3a59c72bae
Alex Junior <alex.s.jr@outlook.com>
2022-09-02 16:12:23 -0300

    SPA finalizado

--- conFusion/src/app/app-routing/routes.ts
+++ conFusion/src/app/app-routing/routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-
 import { MenuComponent } from '../menu/menu.component';
 import { DishdetailComponent } from '../dishdetail/dishdetail.component';
 import { HomeComponent } from '../home/home.component';
@@ -10,4 +9,5 @@ export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'menu', component: MenuComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'contactus', component: ContactComponent },
 ];
